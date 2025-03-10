8acad75aa122c9abe98eae71b46204d230bcfc9f
Swarup Swaraj <swarupswaraj369@gmail.com>
2025-03-10 21:50:35 +0530

    Questions Solved : 2/100

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -1,27 +1,32 @@
 package LeetCode_Questions;
 import java.util.*;
+
 public class ContainerWithMostWater {
-    public static int maxArea(ArrayList<Integer>height){
-        int maxWater = 0 ;
-        int lp = 0 ;
-        int rp = height.size()-1;
-        while (lp<rp) {
-            int ht = Math.min(height.get(lp), height.get(rp));
-            int width = rp-lp ;
-            int currWater = ht*width ;
+    public static int maxArea(int[] height) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.length - 1;
+
+        while (lp < rp) {
+            int ht = Math.min(height[lp], height[rp]);
+            int width = rp - lp;
+            int currWater = ht * width;
             maxWater = Math.max(maxWater, currWater);
-            // update the pointer 
-            if(height.get(lp) < height.get(rp)){
+
+            // Update pointers
+            if (height[lp] < height[rp]) {
                 lp++;
-            }else{
+            } else {
                 rp--;
-            }    
+            }
         }
         return maxWater;
     }
-    public static void main(String[] args) {
-        ArrayList<Integer> height = new ArrayList<>(Arrays.asList(1,8,6,2,5,4,8,3,7));
-        System.out.println(maxArea(height));
 
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7}; // Use an array instead of ArrayList
+
+        int result = maxArea(height);
+        System.out.println("Maximum water that can be stored: " + result);
     }
 }
