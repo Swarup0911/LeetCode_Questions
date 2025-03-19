34304202fe4476a5823c6bbfaa27692f357b3aac
Swarup Swaraj <swarupswaraj369@gmail.com>
2025-03-19 22:41:33 +0530

    Update firstOccurenceinString.java

---
+++ firstOccurenceinString.java
@@ -0,0 +1,22 @@
+package LeetCode_Questions;
+
+public class firstOccurenceinString {
+    public int str(String haystack, String needle) { // A method that uses inbuilt methods to find the answer 
+           return haystack.indexOf(needle) ;
+    }  
+    public static int strStr(String haystack, String needle) {
+        for(int i = 0 ; i<(haystack.length()-needle.length())+1 ; i++){
+            if(haystack.charAt(i) == needle.charAt(0)){
+                if(haystack.substring(i , needle.length()+i).equals(needle)){
+                    return i ;
+                }
+            }
+        }
+        return -1 ;
+    }
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+        System.out.println(strStr(haystack, needle));
+    }
+}
